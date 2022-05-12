e17d758f000bc97103445dcabba3400d8555c2ac
supernagibator3000 <foxsfoxs765@gmail.com>
2022-05-12 15:11:47 +0300

    prepare for client

--- src/main/java/com/superngb/monitoring_system/Configs/MvcConfig.java
+++ src/main/java/com/superngb/monitoring_system/Configs/MvcConfig.java
@@ -9,17 +9,17 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/profile").setViewName("profile");
-        registry.addViewController("/admin").setViewName("admin/admin");
-        registry.addViewController("/admin/personalities").setViewName("admin/admin_personalities");
-        registry.addViewController("/admin/users").setViewName("admin/admin_users");
-        registry.addViewController("/admin/teachers").setViewName("admin/admin_teachers");
-        registry.addViewController("/admin/students").setViewName("admin/admin_students");
-        registry.addViewController("/admin/groups").setViewName("admin/admin_groups");
-        registry.addViewController("/admin/subjects").setViewName("admin/admin_subjects");
-        registry.addViewController("/admin/events").setViewName("admin/admin_events");
-        registry.addViewController("/teacher").setViewName("teacher/teacher");
-        registry.addViewController("/teacher/subjects").setViewName("teacher/teacher_subjects");
+//        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/profile").setViewName("profile");
+//        registry.addViewController("/admin").setViewName("admin/admin");
+//        registry.addViewController("/admin/personalities").setViewName("admin/admin_personalities");
+//        registry.addViewController("/admin/users").setViewName("admin/admin_users");
+//        registry.addViewController("/admin/teachers").setViewName("admin/admin_teachers");
+//        registry.addViewController("/admin/students").setViewName("admin/admin_students");
+//        registry.addViewController("/admin/groups").setViewName("admin/admin_groups");
+//        registry.addViewController("/admin/subjects").setViewName("admin/admin_subjects");
+//        registry.addViewController("/admin/events").setViewName("admin/admin_events");
+//        registry.addViewController("/teacher").setViewName("teacher/teacher");
+//        registry.addViewController("/teacher/subjects").setViewName("teacher/teacher_subjects");
     }
 }
