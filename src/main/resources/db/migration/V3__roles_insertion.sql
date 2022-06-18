3c7fcf3a084b8c64e790618c7f0297e83b9c45a2
supernagibator3000 <foxsfoxs765@gmail.com>
2022-06-18 21:23:11 +0300

    remove flyway

--- src/main/resources/db/migration/V3__roles_insertion.sql
+++
@@ -1,2 +0,0 @@
-insert into roles (id, name) values (1, 'ROLE_USER');
-insert into roles (id, name) values (2, 'ROLE_ADMIN');
--- src/main/resources/db/migration/V5__encrypt_secrets.sql
+++
@@ -1,2 +0,0 @@
-create extension if not exists pgcrypto;
-update users set password = crypt(password, gen_salt('bf', 10));
